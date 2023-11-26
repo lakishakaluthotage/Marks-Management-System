cccb19f92fbfaa14ae507365e3e2728aaf72faa7
Lakisha Kaluthotage <95086455+lakishakaluthotage@users.noreply.github.com>
2023-11-26 07:23:10 +0530

    Update Part 1.py

--- Part 1.py
+++ Part 1.py
@@ -1,7 +1,6 @@
 # I declare that my work contains no examples of misconduct, such as plagiarism, or collusion. 
 # Any code taken from other sources is referenced within my code solution. 
-# Student ID: 20210372 
-# Date: 06.12.2021
+
 
 print("Progression outcomes of Students.\n")
 
