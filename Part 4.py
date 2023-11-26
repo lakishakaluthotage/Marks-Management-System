30d51422631bf8f7321674e5f0451a3bc86619ba
Lakisha Kaluthotage <95086455+lakishakaluthotage@users.noreply.github.com>
2023-11-26 07:24:22 +0530

    Update Part 4.py

--- Part 4.py
+++ Part 4.py
@@ -1,8 +1,3 @@
-# I declare that my work contains no examples of misconduct, such as plagiarism, or collusion. 
-# Any code taken from other sources is referenced within my code solution. 
-# Student ID: 20210372
-# Date: 06.12.2021
-
 print("Staff version with list of accessed data from the text file!\n")
 # declaring variables and assigning initial values
 total = 0
