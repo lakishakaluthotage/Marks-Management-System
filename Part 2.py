7c88eaefe040c686e065cc2c914c2f5d42352651
Lakisha Kaluthotage <95086455+lakishakaluthotage@users.noreply.github.com>
2023-11-26 07:23:49 +0530

    Update Part 2.py

--- Part 2.py
+++ Part 2.py
@@ -1,8 +1,3 @@
-# I declare that my work contains no examples of misconduct, such as plagiarism, or collusion. 
-# Any code taken from other sources is referenced within my code solution. 
-# Student ID: 20210372 
-# Date: 06.12.2021
-
 print("Staff version with Vertical Histogram.\n")
 
 #declaring variables and assigning initial values
